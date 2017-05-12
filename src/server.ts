f9a27fb46898e40c5e6125d0f0983f3b125a9fab
danielweck <daniel.weck@gmail.com>
2017-05-12 08:00:34 +0100

    further server split

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,15 @@
 import * as crypto from "crypto";
 import * as path from "path";
 import * as querystring from "querystring";
-import * as util from "util";
 
 import * as express from "express";
 import * as mime from "mime-types";
 import * as morgan from "morgan";
-import { JSON } from "ta-json";
 
 import { Link } from "./models/publication-link";
 import { EpubParser } from "./parser/epub";
+import { serverManifestJson } from "./server-manifestjson";
 import { serverMediaOverlays } from "./server-mediaoverlays";
-import { sortObject } from "./utils";
 
 export function launchServer(filePath: string) {
 
@@ -67,160 +65,6 @@ export function launchServer(filePath: string) {
 
     server.use("/readerNYPL", express.static("reader-NYPL"));
     server.use("/readerHADRIEN", express.static("reader-HADRIEN"));
-
-    const routerManifestJson = express.Router();
-    // routerManifestJson.use(morgan("combined"));
-
-    routerManifestJson.get(["/", "/show/:jsonPath?"],
-        (req: express.Request, res: express.Response) => {
-
-            if (!req.params.pathBase64) {
-                req.params.pathBase64 = (req as any).pathBase64;
-            }
-
-            const pathBase64Str = new Buffer(req.params.pathBase64, "base64").toString("utf8");
-
-            EpubParser.load(filePath)
-                .then((publication) => {
-                    console.log("== EpubParser: resolve");
-                    // dumpPublication(publication);
-
-                    // console.log(req.url); // path local to this router
-                    // console.log(req.baseUrl); // path local to above this router
-                    // console.log(req.originalUrl); // full path (req.baseUrl + req.url)
-                    // url.parse(req.originalUrl, false).host
-                    // req.headers.host has port, not req.hostname
-
-                    const rootUrl = "http://" + req.headers.host + "/pub/" + req.params.pathBase64;
-                    const manifestURL = rootUrl + "/manifest.json";
-                    publication.AddLink("application/webpub+json", ["self"], manifestURL, false);
-
-                    let hasMO = false;
-                    if (publication.Spine) {
-                        const link = publication.Spine.find((l) => {
-                            if (l.Properties && l.Properties.MediaOverlay) {
-                                return true;
-                            }
-                            return false;
-                        });
-                        if (link) {
-                            hasMO = true;
-                        }
-                    }
-                    if (hasMO) {
-                        const moURL = rootUrl + "/" + EpubParser.mediaOverlayURLPath +
-                            "?" + EpubParser.mediaOverlayURLParam + "={path}";
-                        publication.AddLink("application/vnd.readium.mo+json", ["media-overlay"], moURL, true);
-                    }
-                    if (req.url.indexOf("/show") >= 0) {
-                        let objToSerialize: any = null;
-
-                        if (req.params.jsonPath) {
-                            switch (req.params.jsonPath) {
-
-                                case "all": {
-                                    objToSerialize = publication;
-                                    break;
-                                }
-                                case "cover": {
-                                    objToSerialize = publication.GetCover();
-                                    break;
-                                }
-                                case "mediaoverlays": {
-                                    objToSerialize = publication.FindAllMediaOverlay();
-                                    break;
-                                }
-                                case "spine": {
-                                    objToSerialize = publication.Spine;
-                                    break;
-                                }
-                                case "pagelist": {
-                                    objToSerialize = publication.PageList;
-                                    break;
-                                }
-                                case "landmarks": {
-                                    objToSerialize = publication.Landmarks;
-                                    break;
-                                }
-                                case "links": {
-                                    objToSerialize = publication.Links;
-                                    break;
-                                }
-                                case "resources": {
-                                    objToSerialize = publication.Resources;
-                                    break;
-                                }
-                                case "toc": {
-                                    objToSerialize = publication.TOC;
-                                    break;
-                                }
-                                case "metadata": {
-                                    objToSerialize = publication.Metadata;
-                                    break;
-                                }
-                                default: {
-                                    objToSerialize = null;
-                                }
-                            }
-                        } else {
-                            objToSerialize = publication;
-                        }
-
-                        if (!objToSerialize) {
-                            objToSerialize = {};
-                        }
-
-                        const jsonObj = JSON.serialize(objToSerialize);
-                        const jsonStr = global.JSON.stringify(jsonObj, null, "    ");
-
-                        // breakLength: 100  maxArrayLength: undefined
-                        const dumpStr = util.inspect(objToSerialize,
-                            { showHidden: false, depth: 1000, colors: false, customInspect: true });
-
-                        res.status(200).send("<html><body>" +
-                            "<h2>" + pathBase64Str + "</h2>" +
-                            "<p><pre>" + jsonStr + "</pre></p>" +
-                            "<p><pre>" + dumpStr + "</pre></p>" +
-                            "</body></html>");
-                    } else {
-                        res.setHeader("Access-Control-Allow-Origin", "*");
-                        res.set("Content-Type", "application/webpub+json; charset=utf-8");
-
-                        const publicationJsonObj = JSON.serialize(publication);
-                        const publicationJsonStr = global.JSON.stringify(sortObject(publicationJsonObj), null, "");
-
-                        const checkSum = crypto.createHash("sha256");
-                        checkSum.update(publicationJsonStr);
-                        const hash = checkSum.digest("hex");
-
-                        const match = req.header("If-None-Match");
-                        if (match === hash) {
-                            res.status(304); // StatusNotModified
-                            return;
-                        }
-
-                        res.setHeader("ETag", hash);
-
-                        const links = publication.GetPreFetchResources();
-                        if (links && links.length) {
-                            let prefetch = "";
-                            links.forEach((l) => {
-                                prefetch += "<" + l.Href + ">;" + "rel=prefetch,";
-                            });
-
-                            res.setHeader("Link", prefetch);
-                        }
-
-                        // res.setHeader("Cache-Control", "public,max-age=86400");
-
-                        res.status(200).send(publicationJsonStr);
-                    }
-                }).catch((err) => {
-                    console.log("== EpubParser: reject");
-                    console.log(err);
-                    res.status(500).send("<html><body><p>Internal Server Error</p><p>" + err + "</p></body></html>");
-                });
-        });
 
     const routerAssets = express.Router();
     // routerAssets.use(morgan("combined"));
@@ -447,7 +291,7 @@ export function launchServer(filePath: string) {
         next();
     });
 
-    routerPathBase64.use("/:pathBase64/manifest.json", routerManifestJson);
+    serverManifestJson(routerPathBase64, filePath);
     serverMediaOverlays(routerPathBase64, filePath);
     routerPathBase64.use("/:pathBase64/:asset(*)", routerAssets);
     routerPathBase64.get("/:pathBase64", (_req: express.Request, res: express.Response) => {
