23175bb80ee6b5c9b548c01d1b3930488aea8d70
danielweck <daniel.weck@gmail.com>
2017-06-29 17:18:06 +0100

    removed now-useless comments (debug)

--- src/parser/publication-parser.ts
+++ src/parser/publication-parser.ts
@@ -8,12 +8,7 @@ export async function PublicationParsePromise(filePath: string): Promise<Publica
 
     const fileName = path.basename(filePath);
     const ext = path.extname(fileName).toLowerCase();
-    const check = /\.epub[3]?$/.test(ext);
-    console.log(check);
-    console.log(ext);
-    console.log(fileName);
-    console.log(filePath);
-    return check ?
+    return /\.epub[3]?$/.test(ext) ?
         EpubParsePromise(filePath) :
         CbzParsePromise(filePath);
 }
