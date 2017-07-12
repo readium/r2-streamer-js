6f5c141d551b4891d52c82797a8cc0b8f64524f0
danielweck <daniel.weck@gmail.com>
2017-07-12 16:26:04 +0100

    minor Electron test app updates

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,13 +1,29 @@
+// https://github.com/electron/electron/blob/master/docs/api/sandbox-option.md
+// https://github.com/electron/electron/blob/master/docs/api/process.md
+// https://github.com/electron/electron/blob/master/docs/api/browser-window.md
+// https://github.com/electron/electron/blob/master/docs/api/protocol.md
+// https://github.com/electron/electron/blob/master/docs/api/web-frame.md
+// https://github.com/electron/electron/blob/master/docs/api/web-contents.md
+// https://github.com/electron/electron/blob/master/docs/api/web-request.md
+// https://github.com/electron/electron/blob/master/docs/api/session.md
+// https://github.com/electron/electron/blob/master/docs/api/webview-tag.md
+// https://github.com/electron/electron/blob/master/docs/api/browser-view.md
+// https://github.com/electron/electron/blob/master/docs/api/client-request.md
+// https://github.com/electron/electron/blob/master/docs/api/sandbox-option.md
+
 import * as fs from "fs";
 import * as path from "path";
 
+import { encodeURIComponent_RFC3986 } from "@utils/http/UrlUtils";
 import * as debug_ from "debug";
-import { BrowserWindow, app } from "electron";
+import { BrowserWindow, app, session } from "electron";
 import * as filehound from "filehound";
 import * as portfinder from "portfinder";
 
 import { Server } from "../http/server";
 import { initGlobals } from "../init-globals";
+
+// import * as mime from "mime-types";
 
 initGlobals();
 
@@ -15,9 +31,44 @@ const debug = debug_("r2:electron:main");
 
 let electronBrowserWindow: Electron.BrowserWindow | undefined;
 
+// protocol.registerStandardSchemes(["epub", "file"], { secure: true });
+
 function createElectronBrowserWindow() {
 
-    debug("Server start, Electron main window ...");
+    debug("createElectronBrowserWindow()");
+
+    // const proto = session.defaultSession.protocol;
+
+    // protocol.registerServiceWorkerSchemes(["epub"]);
+
+    // registerFileProtocol
+    // protocol.registerBufferProtocol("epub",
+    //     (request, callback) => {
+    //         debug(request.url);
+    //         const data = fs.readFileSync(request.url);
+    //         const mimeType = mime.lookup(request.url);
+    //         callback({ data, mimeType });
+    //     }, (error) => {
+    //         debug(error);
+    //     });
+
+    if (session.defaultSession) {
+        session.defaultSession.clearStorageData({
+            quotas: [
+                "temporary",
+                "persistent",
+                "syncable"],
+            storages: [
+                "appcache",
+                "cookies",
+                "filesystem",
+                "indexdb",
+                "localstorage",
+                "shadercache",
+                "websql",
+                "serviceworkers"],
+        });
+    }
 
     // tslint:disable-next-line:no-floating-promises
     (async () => {
@@ -38,13 +89,34 @@ function createElectronBrowserWindow() {
         });
         debug(pubManifestUrls);
 
-        electronBrowserWindow = new BrowserWindow({ width: 800, height: 600 });
+        electronBrowserWindow = new BrowserWindow({
+            height: 600,
+            webPreferences: {
+                allowRunningInsecureContent: false,
+                contextIsolation: false,
+                devTools: true,
+                nodeIntegration: true,
+                nodeIntegrationInWorker: true,
+                sandbox: false,
+                webSecurity: true,
+                // preload: __dirname + "/" + "preload.js",
+            },
+            width: 800,
+        });
 
-        electronBrowserWindow.loadURL(url);
-        electronBrowserWindow.webContents.openDevTools();
+        // electronBrowserWindow.loadURL(url);
+        const urlEncoded = encodeURIComponent_RFC3986(url);
+        electronBrowserWindow.loadURL(`file://${__dirname}/index.html?pub=${urlEncoded}`);
+
+        electronBrowserWindow.webContents.on("dom-ready", () => {
+            debug("electronBrowserWindow dom-ready");
+            if (electronBrowserWindow) {
+                electronBrowserWindow.webContents.openDevTools();
+            }
+        });
 
         electronBrowserWindow.on("closed", () => {
-            debug("Server stop ...");
+            debug("electronBrowserWindow closed");
             electronBrowserWindow = undefined;
             server.stop();
         });
@@ -52,16 +124,19 @@ function createElectronBrowserWindow() {
 }
 
 app.on("window-all-closed", () => {
+    debug("app window-all-closed");
     if (process.platform !== "darwin") {
         app.quit();
     }
 });
 
 app.on("ready", () => {
+    debug("app ready");
     createElectronBrowserWindow();
 });
 
 app.on("activate", () => {
+    debug("app activate");
     if (!electronBrowserWindow) {
         createElectronBrowserWindow();
     }
