32c83b7e861fa13487102f6e9a82a396773ba9f4
danielweck <daniel.weck@gmail.com>
2017-10-23 12:49:54 +0100

    added "page turn" events (nav) and comments

--- src/electron/common/events.ts
+++ src/electron/common/events.ts
@@ -1,10 +1,34 @@
+
+// in RENDERER: ipcRenderer.send()
+// in MAIN: ipcMain.on()
 export const R2_EVENT_TRY_LCP_PASS = "R2_EVENT_TRY_LCP_PASS";
+
+// in MAIN: event.sender.send(), where event is from the above ipcMain.on()
+// in RENDERER: ipcRenderer.on()
 export const R2_EVENT_TRY_LCP_PASS_RES = "R2_EVENT_TRY_LCP_PASS_RES";
 
-export const R2_EVENT_DEVTOOLS = "R2_EVENT_DEVTOOLS";
-
+// in RENDERER: webview.send()
+// in WEBVIEW: ipcRenderer.on()
 export const R2_EVENT_READIUMCSS = "R2_EVENT_READIUMCSS";
 
+// in RENDERER: webview.send()
+// in WEBVIEW: ipcRenderer.on()
+export const R2_EVENT_PAGE_TURN = "R2_EVENT_PAGE_TURN";
+
+// in WEBVIEW: ipcRenderer.sendToHost()
+// in RENDERER: webview.addEventListener("ipc-message")
+export const R2_EVENT_PAGE_TURN_RES = "R2_EVENT_PAGE_TURN_RES";
+
+// in MAIN: browserWindow.webContents.send(()
+// in RENDERER: ipcRenderer.on()
+// in WEBVIEW: ipcRenderer.sendToHost()
+// in RENDERER: webview.addEventListener("ipc-message")
 export const R2_EVENT_LINK = "R2_EVENT_LINK";
 
+// in WEBVIEW: ipcRenderer.sendToHost()
+// in RENDERER: webview.addEventListener("ipc-message")
 export const R2_EVENT_WEBVIEW_READY = "R2_EVENT_WEBVIEW_READY";
+
+// in RENDERER: ipcRenderer.send()
+// in MAIN: ipcMain.on()
+export const R2_EVENT_DEVTOOLS = "R2_EVENT_DEVTOOLS";
