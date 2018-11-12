fc7dc0ca3adeebd1d43d16b7345bcc77924b5231
Daniel Weck <daniel.weck@gmail.com>
2018-11-12 12:43:28 +0000

    Git revision JSON now includes version info about NodeJS and NPM build environment

--- tools/gitrev.js
+++ tools/gitrev.js
@@ -8,42 +8,64 @@ let json = {
     "urlDiff": "NOT A GIT REPO?",
 };
 
-try {
-    var git = require("git-rev-sync");
+var exec = require("child_process").exec;
+exec("npm --version", function(execError, stdin, stderr) {
+    var npmVersion = "?";
+    if (execError) {
+        console.log(execError);
 
-    var fs = require("fs");
-    var path = require("path");
-
-    // console.log("process.cwd():");
-    // console.log(process.cwd());
-    // console.log("path.resolve(\".\")");
-    // console.log(path.resolve("."));
-
-    // console.log("__dirname:");
-    // console.log(__dirname);
-
-    // const args = process.argv.slice(2);
-    // console.log("args:");
-    // console.log(args);
-
-    const detached = "Detached: ";
-    let branch = git.branch();
-    if (branch.indexOf(detached) === 0) {
-        branch = branch.substr(detached.length);
+        if (execError.code === 127) {
+            console.log("NPM not found?");
+        }
+        if (stderr) {
+            console.log(stderr.trim());
+        }
+        if (stdin) {
+            console.log(stdin.trim());
+        }
+    } else {
+        npmVersion = stdin.toString().trim(); // .split('\n')[0].trim();
     }
-    const gitUrlBase = "https://github.com/readium/r2-streamer-js/";
-    json = {
-        "short": git.short(),
-        "long": git.long(),
-        "branch": branch,
-        "date": git.date(),
-        "dirty": git.isTagDirty(),
-    };
-    json["urlHistory"] = gitUrlBase + "commits/" + json.long;
-    json["urlDiff"] = gitUrlBase + "compare/" + json.long + "..." + json.branch;
 
-    // relative to process.cwd(), not __dirname
-    fs.writeFileSync("./dist/gitrev.json", JSON.stringify(json, null, "  "), "utf8");
-} catch (err) {
-    console.log(err);
-}
+    try {
+        var git = require("git-rev-sync");
+
+        var fs = require("fs");
+        var path = require("path");
+
+        // console.log("process.cwd():");
+        // console.log(process.cwd());
+        // console.log("path.resolve(\".\")");
+        // console.log(path.resolve("."));
+
+        // console.log("__dirname:");
+        // console.log(__dirname);
+
+        // const args = process.argv.slice(2);
+        // console.log("args:");
+        // console.log(args);
+
+        const detached = "Detached: ";
+        let branch = git.branch();
+        if (branch.indexOf(detached) === 0) {
+            branch = branch.substr(detached.length);
+        }
+        const gitUrlBase = "https://github.com/readium/r2-streamer-js/";
+        json = {
+            "node": process.version.replace("v", ""),
+            "npm": npmVersion,
+            "short": git.short(),
+            "long": git.long(),
+            "branch": branch,
+            "date": git.date(),
+            // "dirty": git.isTagDirty(),
+        };
+        json["urlHistory"] = gitUrlBase + "commits/" + json.long;
+        json["urlDiff"] = gitUrlBase + "compare/" + json.long + "..." + json.branch;
+
+        // relative to process.cwd(), not __dirname
+        fs.writeFileSync("./dist/gitrev.json", JSON.stringify(json, null, "  "), "utf8");
+    } catch (err) {
+        console.log(err);
+    }
+});
