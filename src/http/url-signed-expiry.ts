a7544a09903a4e0853df994f115dbb6425fbd74d
Daniel Weck <daniel.weck@gmail.com>
2022-03-09 12:27:05 +0000

    fix: signed expiry on resource URLs now bound to asset path

--- src/http/url-signed-expiry.ts
+++ src/http/url-signed-expiry.ts
@@ -43,7 +43,7 @@ const computeHashSecret = () => {
 //    number of seconds
 //    (default is 86400 === 24h)
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const signExpiringResourceURLs = (_rootUrl: string, pathBase64Str: string, jsonObj: any): string | undefined => {
+export const signExpiringResourceURLs = (rootUrl: string, pathBase64Str: string, jsonObj: any) => {
 
     // debug(`pathBase64Str: ${pathBase64Str}`);
     // reqparams.pathBase64 is already decoded!
@@ -52,7 +52,7 @@ export const signExpiringResourceURLs = (_rootUrl: string, pathBase64Str: string
 
     const hashSecret = computeHashSecret();
     if (!hashSecret) {
-        return undefined;
+        return;
     }
 
     // milliseconds since epoch (midnight, 1 Jan 1970)
@@ -75,37 +75,43 @@ export const signExpiringResourceURLs = (_rootUrl: string, pathBase64Str: string
     }
     // debug(`URL expiry: ${expiry}`);
 
-    const checkSumData = crypto.createHash("sha256");
-    checkSumData.update(`${timestamp}_${hashSecret}_${expiry}_${pathBase64Str}`);
-    const hashData = checkSumData.digest("hex");
-
-    const queryParamJson = {
-        timestamp, // milliseconds
-        expiry, // seconds
-        hash: hashData,
-    };
-
-    const queryParamValue = Buffer.from(JSON.stringify(queryParamJson)).toString("base64");
-
     traverseJsonObjects(jsonObj,
         (obj) => {
             if (obj.href && typeof obj.href === "string"
                 && !/^https?:\/\//.test(obj.href) && !/^data:\/\//.test(obj.href)) {
-                // obj.href_ = obj.href;
 
-                const fakeRootUrl = "https://dummy.com/";
-                const url = new URL(obj.href, fakeRootUrl); // `${rootUrl}/manifest.json`
+                // EXAMPLES:
+                // obj.href === "./EPUB/path/to/audio.mp3#t=123" (TOC)
+                // obj.href === "EPUB/path/to/chapter.html" (spine/reading-order, or resources collection)
+
+                const publicationRootUrl = new URL(`${rootUrl}/`); // manifest.json
+                // debug(`------------ publicationRootUrl: ${publicationRootUrl.toString()}`);
+                const url = new URL(obj.href, publicationRootUrl);
+                const resourcePath = url.pathname.replace(publicationRootUrl.pathname, "");
+                // debug(`------------ Link HREF (absolute URL): ${obj.href} => ${url.toString()} (${resourcePath})`);
+
+                const checkSumData = crypto.createHash("sha256");
+                checkSumData.update(`${timestamp}_${hashSecret}_${resourcePath}_${expiry}_${pathBase64Str}`);
+                const hashData = checkSumData.digest("hex");
+
+                const queryParamJson = {
+                    hash: hashData,
+                    timestamp, // milliseconds
+                    expiry, // seconds
+                };
+
+                const queryParamValue = Buffer.from(JSON.stringify(queryParamJson)).toString("base64");
+
                 // implicit encodeURIComponent_RFC3986()
                 url.searchParams.append(URL_SIGNED_EXPIRY_QUERY_PARAM_NAME, queryParamValue);
-                obj.href = url.toString().replace(fakeRootUrl, "");
-                // debug(`------------ Link HREF: ${obj.href}`);
+                // debug(`======= Link HREF (BEFORE): ${obj.href}`);
+                obj.href = url.toString().replace(publicationRootUrl.toString(), ""); // make relative again
+                // debug(`======= Link HREF (AFTER): ${obj.href}`);
             }
         });
-
-    return queryParamValue;
 };
 
-export const verifyExpiringResourceURL = (queryParamValue: string | undefined, pathBase64Str: string, _pathInZip: string): boolean => {
+export const verifyExpiringResourceURL = (queryParamValue: string | undefined, pathBase64Str: string, pathInZip: string): boolean => {
 
     const hashSecret = computeHashSecret();
     if (!hashSecret) {
@@ -124,18 +130,19 @@ export const verifyExpiringResourceURL = (queryParamValue: string | undefined, p
             return false;
         }
 
+        const resourcePath = pathInZip;
         const checkSumData = crypto.createHash("sha256");
-        checkSumData.update(`${json.timestamp}_${hashSecret}_${json.expiry}_${pathBase64Str}`);
+        checkSumData.update(`${json.timestamp}_${hashSecret}_${resourcePath}_${json.expiry}_${pathBase64Str}`);
         const hashData = checkSumData.digest("hex");
         if (hashData !== json.hash) {
-            debug(`HASH diff! ${hashData} !== ${json.hash})`);
+            debug(`HASH diff! ${hashData} !== ${json.hash} (${resourcePath})`);
             return false;
         }
 
         const timestamp = Date.now(); // +new Date()
         const timeDiff = timestamp - json.timestamp;
         if (timeDiff > (json.expiry * 1000)) {
-            debug(`Resource EXPIRED! ${timeDiff} > ${json.expiry})`);
+            debug(`Resource EXPIRED! ${timeDiff} > ${json.expiry} (${resourcePath})`);
             return false;
         }
 
